935d71acab6dbd93877e11059775807417aa11dc
jw5367 <zzss9921@naver.com>
2021-11-05 19:03:53 +0900

    Day 5

--- 10952.js
+++ 10952.js
@@ -1,2 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync("./text.txt").toString().trim().split("\n");
+
+while(input[0][0] != 0) {
+ const numbers = input.shift().split(" ");
+ console.log(+numbers[0] + +numbers[1]);
+}
