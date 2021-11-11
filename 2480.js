52a37080d8b6008de479c7f270812aabc28b7c27
jw5367 <zzss9921@naver.com>
2021-11-11 23:01:38 +0900

    Day 9

--- 2480.js
+++ 2480.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
-const input = fs.readFileSync("./text.txt").toString().trim().split(" ");
+const input = fs.readFileSync("./text.txt").toString().trim().split(" ").map(Number);
 
+let inputNum = input.sort((a, b) => a - b);
+const [A, B, C] = [inputNum[0], inputNum[1], inputNum[2]];
+let value;
+
+if(A===B && B===C ){
+ value = 10000 + A *1000;
+}else if(A===B || B===C){
+  value = 1000 + B * 100;
+}else if(A !== B && B !== C){
+  value = C * 100;
+}
+
+console.log(value);
---
+++ 8958.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync("./text.txt").toString().trim().split("\n");
+
+for(i = 0; i <= input[0]; i++){
+  for(j = 0; j <= input[i].length; j++){
+    
+  }
+}
