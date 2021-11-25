8ccc4f4794d14707c78f9e9715ee4e99a207c700
jw5367 <zzss9921@naver.com>
2021-11-25 16:50:55 +0900

    Day 15

--- 2442.js
+++ 2442.js
@@ -4,16 +4,13 @@ const input = fs.readFileSync("./text.txt").toString().trim();
 let num = Number(input);
 let value = '';
 
-for(i = 0; i < Number(input); i++){
-
-  for(j = 0 ; j < num; j++){
+for(i = 0; i < num; i++){
+  for(j = 1; j < num - i; j++){
+    value += ' ';
+  }
+  for(j = 0; j < i * 2 + 1; j++){
     value += '*';
   }
   value += '\n';
-  for(j = 0; j <= i; j++ ){
-    value += ' ';
-  }
-  num = num - 1;
 }
 process.stdout.write(value);
-
--- 2443.js
+++ 2443.js
@@ -4,16 +4,13 @@ const input = fs.readFileSync("./text.txt").toString().trim();
 let num = Number(input);
 let value = '';
 
-for(i = 0; i < Number(input); i++){
-
-  for(j = 0 ; j < num; j++){
+for(i = 1; i <= num; i++){
+  for(j = 0; j < (num * 2) - (i * 2 - 1); j++){
     value += '*';
   }
   value += '\n';
-  for(j = 0; j <= i; j++ ){
+  for(j = 0; j < i; j++){
     value += ' ';
   }
-  num = num - 1;
 }
 process.stdout.write(value);
-
