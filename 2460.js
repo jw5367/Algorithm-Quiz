546ec2ae7964e8184d18453b53409939ff12408f
jw5367 <zzss9921@naver.com>
2021-12-12 17:25:04 +0900

    Day 23

--- 2460.js
+++ 2460.js
@@ -1,3 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync("./text.txt").toString().trim().split("\n");
 
+let plusPeople = 0;
+let array = [];
+
+for(i = 0; i < input.length; i++){
+  let people = input[i].split(' ').map(Number);
+  plusPeople += people[1];
+  plusPeople -= people[0];
+  array.push(plusPeople);
+  array.sort((a, b) => a - b);
+}
+console.log(array[9]);
